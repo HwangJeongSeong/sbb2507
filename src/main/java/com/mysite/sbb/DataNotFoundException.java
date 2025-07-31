453e673031df400e75cced6d9e9c1c17229526da
jeongseong <hjs54336297@gmail.com>
2025-07-31 15:16:57 +0900

    exception

---
+++ src/main/java/com/mysite/sbb/DataNotFoundException.java
@@ -0,0 +1,12 @@
+package com.mysite.sbb;
+
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus
+public class DataNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public DataNotFoundException(String message) {
+        super(message);
+    }
+}
